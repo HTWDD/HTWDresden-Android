aa88c1bad57bf0509d58ce088565b62fabf3ec4a
Kay Förster <red.mail2@gmail.com>
2016-01-26 17:56:25 +0100

    EventBus auf alle Threads weiterleiten

--- app/src/main/java/de/htwdd/htwdresden/classes/EventBus.java
+++ app/src/main/java/de/htwdd/htwdresden/classes/EventBus.java
@@ -1,6 +1,7 @@
 package de.htwdd.htwdresden.classes;
 
 import com.squareup.otto.Bus;
+import com.squareup.otto.ThreadEnforcer;
 
 /**
  * EventBus zur internen Benachrichtigung über Ereignisse
@@ -8,7 +9,7 @@ import com.squareup.otto.Bus;
  * @author Kay Förster
  */
 public class EventBus {
-    private static final Bus BUS = new Bus();
+    private static final Bus BUS = new Bus(ThreadEnforcer.ANY);
 
     public static Bus getInstance() {
         return BUS;
