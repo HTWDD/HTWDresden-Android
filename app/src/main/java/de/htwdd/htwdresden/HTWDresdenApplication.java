7c61765f46bc8c9d6403d859ceb0e653a94e26cb
Kay Förster <red.mail2@gmail.com>
2016-02-24 19:19:01 +0100

    ACRA Crash Report hinzugefügt

---
+++ app/src/main/java/de/htwdd/htwdresden/HTWDresdenApplication.java
@@ -0,0 +1,47 @@
+package de.htwdd.htwdresden;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(
+        mode = ReportingInteractionMode.DIALOG,
+        mailTo = "htwcampusapp@htw-dresden.de",
+        resDialogTitle = R.string.app_name,
+        resDialogIcon = R.drawable.ic_warning_24dp,
+        resDialogText = R.string.crash_dialog_text,
+        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. When defined, adds a user text field input with this text resource as a label
+        resDialogOkToast = R.string.crash_dialog_ok_toast, // optional. displays a Toast message when the user accepts to send a report.
+        customReportContent = {
+                ReportField.APP_VERSION_CODE,
+                ReportField.APP_VERSION_NAME,
+                ReportField.ANDROID_VERSION,
+                ReportField.STACK_TRACE,
+                ReportField.LOGCAT,
+                ReportField.USER_COMMENT
+        },
+        sharedPreferencesName = "de.htwdd.htwdresden_preferences"
+)
+public class HTWDresdenApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Setze Einstellungen beim ersten Starten der App
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        if (sharedPreferences.getBoolean("FIRST_RUN", true)){
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("FIRST_RUN", false);
+            editor.putBoolean("acra.enable", true);
+            editor.apply();
+        }
+
+        // Arca starten
+        ACRA.init(this);
+    }
+}
