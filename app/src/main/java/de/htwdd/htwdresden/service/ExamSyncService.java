97a34cdbd0c7106f8a917f374a7ea6f12810c758
Kay Förster <red.mail2@gmail.com>
2017-04-02 17:25:07 +0200

    ErrorLister in AbstractSyncHelper verlagert

--- app/src/main/java/de/htwdd/htwdresden/service/ExamSyncService.java
+++ app/src/main/java/de/htwdd/htwdresden/service/ExamSyncService.java
@@ -9,7 +9,6 @@ import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,36 +31,10 @@ import io.realm.Realm;
 public class ExamSyncService extends AbstractSyncHelper {
     public final static String INTENT_SYNC_EXAMS = "de.htwdd.htwdresden.exams";
     private final static String LOG_TAG = "ExamSyncService";
-    private Stack<JSONArray> results = new Stack<>();
+    private final Stack<JSONArray> results = new Stack<>();
     // Zugangsdaten
     String sNummer;
     String rzLogin;
-    // Error Listener
-    private final Response.ErrorListener errorListener = new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(final VolleyError error) {
-            // Bestimme Fehlermeldung
-            final String message;
-            switch (VolleyDownloader.getResponseCode(error)) {
-                case Const.internet.HTTP_TIMEOUT:
-                    message = getString(R.string.info_internet_timeout);
-                    break;
-                case Const.internet.HTTP_NO_CONNECTION:
-                case Const.internet.HTTP_NOT_FOUND:
-                    message = getString(R.string.info_internet_no_connection);
-                    break;
-                case Const.internet.HTTP_UNAUTHORIZED:
-                    message = getString(R.string.exams_result_wrong_auth);
-                    break;
-                case Const.internet.HTTP_NETWORK_ERROR:
-                default:
-                    message = getString(R.string.info_internet_error);
-            }
-            setError(message);
-            queueCount.decrementCountQueue();
-            Log.e(LOG_TAG, "[Fehler] Konnte Noten nicht abrufen: " + error.toString());
-        }
-    };
 
     public ExamSyncService() {
         super("ExamSyncService", INTENT_SYNC_EXAMS);
@@ -78,12 +51,23 @@ public class ExamSyncService extends AbstractSyncHelper {
         // Auf fertigstellung warten
         waitForFinish();
         // Ergebnisse speichern
-        if (!isCancel() && broadcastNotifier != null) {
+        if (!isCancel()) {
             final boolean result = saveGrades();
-            if (result) {
+            if (result && broadcastNotifier != null) {
                 broadcastNotifier.notifyStatus(0);
             }
         }
+    }
+
+    @Override
+    void setError(@NonNull final String errorMessage) {
+        // Synchronisation abbrechen
+        setCancelToTrue();
+        // Downloads abbrechen
+        VolleyDownloader.getInstance(context).getRequestQueue().cancelAll(Const.internet.TAG_EXAM_RESULTS);
+        // Benachrichtigung senden
+        if (broadcastNotifier != null)
+            broadcastNotifier.notifyStatus(-1, errorMessage);
     }
 
     /**
@@ -168,20 +152,5 @@ public class ExamSyncService extends AbstractSyncHelper {
             realm.close();
         }
         return true;
-    }
-
-    /**
-     * Behandelt alle Maßnahmen wenn ein Fehler aufgetreten ist
-     *
-     * @param errorMessage Fehlermeldung
-     */
-    private void setError(@NonNull final String errorMessage) {
-        // Synchronisation abbrechen
-        setCancelToTrue();
-        // Downloads abbrechen
-        VolleyDownloader.getInstance(context).getRequestQueue().cancelAll(Const.internet.TAG_EXAM_RESULTS);
-        // Benachrichtigung senden
-        if (broadcastNotifier != null)
-            broadcastNotifier.notifyStatus(-1, errorMessage);
     }
 }
