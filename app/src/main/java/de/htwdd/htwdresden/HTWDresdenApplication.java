c04dd66d257f5d73e769263627036f7b3f3d785b
Kay Förster <red.mail2@gmail.com>
2018-01-28 11:44:59 +0100

    Update ACRA

--- app/src/main/java/de/htwdd/htwdresden/HTWDresdenApplication.java
+++ app/src/main/java/de/htwdd/htwdresden/HTWDresdenApplication.java
@@ -1,14 +1,16 @@
 package de.htwdd.htwdresden;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.IntentFilter;
 
 import com.heinrichreimer.canteenbalance.cardreader.CardBalance;
 
 import org.acra.ACRA;
 import org.acra.ReportField;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraDialog;
+import org.acra.annotation.AcraMailSender;
 
 import de.htwdd.htwdresden.classes.DatabaseMigrations;
 import de.htwdd.htwdresden.classes.PreferencesMigrations;
@@ -16,27 +18,28 @@ import de.htwdd.htwdresden.service.MensaCreditReceiver;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
-@ReportsCrashes(
-        mode = ReportingInteractionMode.DIALOG,
-        mailTo = "app@htw-dresden.de",
-        resDialogTitle = R.string.app_name,
-        resDialogIcon = R.drawable.ic_warning_24dp,
-        resDialogText = R.string.crash_dialog_text,
-        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. When defined, adds a user text field input with this text resource as a label
-        resDialogOkToast = R.string.crash_dialog_ok_toast, // optional. displays a Toast message when the user accepts to send a report.
-        resDialogTheme = R.style.AppTheme,
-        customReportContent = {
-                ReportField.APP_VERSION_CODE,
-                ReportField.APP_VERSION_NAME,
-                ReportField.ANDROID_VERSION,
-                ReportField.STACK_TRACE,
-                ReportField.LOGCAT,
-                ReportField.USER_COMMENT
-        },
-        sharedPreferencesName = "de.htwdd.htwdresden_preferences"
+@AcraCore(
+        buildConfigClass = BuildConfig.class,
+        resReportSendSuccessToast = R.string.crash_dialog_ok_toast,
+        reportContent = {ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.STACK_TRACE, ReportField.LOGCAT, ReportField.USER_COMMENT}
 )
+@AcraDialog(
+        resTitle = R.string.app_name,
+        resText = R.string.crash_dialog_text,
+        resIcon = R.drawable.ic_warning_24dp,
+        resTheme = R.style.AppTheme,
+        resCommentPrompt = R.string.crash_dialog_comment_prompt
+)
+@AcraMailSender(mailTo = "app@htw-dresden.de", subject = R.string.app_name)
 public class HTWDresdenApplication extends Application {
     private MensaCreditReceiver mensaCreditReceiver;
+
+    @Override
+    protected void attachBaseContext(final Context base) {
+        super.attachBaseContext(base);
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
 
     @Override
     public void onCreate() {
@@ -45,9 +48,6 @@ public class HTWDresdenApplication extends Application {
         // Migrationen durchführen
         final PreferencesMigrations preferencesMigrations = new PreferencesMigrations(getApplicationContext());
         preferencesMigrations.migrate();
-
-        // ACRA starten
-        ACRA.init(this);
 
         // Realm initialisieren
         Realm.init(this);
