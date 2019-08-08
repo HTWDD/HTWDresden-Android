ec4f16eaa0071b27c34668d8e6eb8889c9e2c32e
Sven Nowak <nowak@develappers.de>
2019-08-08 10:06:36 +0200

    Animationen + Hinweistext

--- app/src/main/java/de/htwdd/htwdresden/OnBoardActivity.java
+++ app/src/main/java/de/htwdd/htwdresden/OnBoardActivity.java
@@ -25,8 +25,6 @@ public class OnBoardActivity extends TutorialActivity {
     LottieAnimationView ltAnimViewActiveAnalytics;
     LottieAnimationView ltAnimViewInActiveAnalytics;
     LottieAnimationView animationCheckBox;
-    LottieAnimationView animationGreenButton;
-    LottieAnimationView animationRedButton;
     TextView noteCrashlytics;
     Button buttonPrev;
     Button buttonNext;
@@ -49,7 +47,7 @@ public class OnBoardActivity extends TutorialActivity {
                     .setView(R.layout.onboarding_fragment_welcome)
                     .setTitle(getString(R.string.welcome))
                     .setContent(getString(R.string.welcome_text_content))
-                    .setDrawable(R.drawable.htw_logo_round_blue)
+                    .setDrawable(R.drawable.htw_logo_gross)
                     .build());
             // Permission Step
             addFragment(new Step.Builder()
@@ -57,7 +55,7 @@ public class OnBoardActivity extends TutorialActivity {
                     .setTitle(getString(R.string.analytics))
                     .setContent(getString(R.string.analytics_text_content))
                     .setSummary(getString(R.string.analytics_text_summary))
-                    .setDrawable(R.drawable.htw_logo_round_blue)
+                    .setDrawable(R.drawable.htw_logo_gross)
                     .build());
 
             addFragment(new Step.Builder()
@@ -65,14 +63,22 @@ public class OnBoardActivity extends TutorialActivity {
                     .setTitle(getString(R.string.crashlytics))
                     .setContent(getString(R.string.crashlytics_text_content))
                     .setSummary(getString(R.string.crashlytics_text_summary))
-                    .setDrawable(R.drawable.htw_logo_round_blue)
+                    .setDrawable(R.drawable.htw_logo_gross)
                     .build());
 
             addFragment(new Step.Builder()
                     .setTitle(getString(R.string.login_with_img))
                     .setView(R.layout.onboarding_fragment_login)
                     .setContent(getString(R.string.login_text_content))
-                    .setDrawable(R.drawable.htw_logo_round_blue)
+                    .setDrawable(R.drawable.htw_logo_gross)
+                    .setSummary(getString(R.string.login_text_summary))
+                    .build());
+
+            addFragment(new Step.Builder()
+                    .setTitle(getString(R.string.login_with_img))
+                    .setView(R.layout.onboarding_fragment_login)
+                    .setContent(getString(R.string.login_text_content))
+                    .setDrawable(R.drawable.htw_logo_gross)
                     .setSummary(getString(R.string.login_text_summary))
                     .build());
 
@@ -81,7 +87,7 @@ public class OnBoardActivity extends TutorialActivity {
                     .setTitle("Activate Crashlogger?")
                     .setContent("This is content")
                     .setSummary("This is summary")
-                    .setDrawable(R.drawable.htw_logo_round_blue)
+                    .setDrawable(R.drawable.htw_logo_gross)
                     .build());
 
             buttonPrev = findViewById(R.id.prev);
@@ -118,8 +124,6 @@ public class OnBoardActivity extends TutorialActivity {
                     editor.putBoolean("firebase_analytics.enable", true);
                     editor.apply();
 
-                    Boolean test = sharedPreferences.getBoolean("firebase_analytics.enable", false);
-
                     ltAnimViewInActiveAnalytics.setVisibility(View.GONE);
                     ltAnimViewActiveAnalytics.setVisibility(View.VISIBLE);
                     ltAnimViewActiveAnalytics.playAnimation();
@@ -131,33 +135,31 @@ public class OnBoardActivity extends TutorialActivity {
             }
         }
 
-        Boolean test = sharedPreferences.getBoolean("firebase_analytics.enable", false);
-
-
         if(position == 2) {
             if(sharedPreferences.getBoolean("firebase_analytics.enable", false)){
 
                 animationCheckBox = findViewById(R.id.anim_active_crashlytics);
+                noteCrashlytics = findViewById(R.id.notes_crashlytics);
+                noteCrashlytics.setVisibility(View.GONE);
 
                 if(!sharedPreferences.getBoolean("firebase_crashlytics.enable", false)){
                     animationCheckBox.setEnabled(true);
                     animationCheckBox.setAnimation("success_blue.json");
+                    animationCheckBox.setRepeatCount(0);
                     animationCheckBox.playAnimation();
                     animationCheckBox.setOnClickListener(view -> {
                         editor.putBoolean("firebase_crashlytics.enable", true);
                         editor.apply();
                         animationCheckBox.setAnimation("check_mark_success_blue.json");
-                        animationCheckBox.loop(false);
+                        animationCheckBox.setRepeatCount(0);
                         animationCheckBox.playAnimation();
                         animationCheckBox.setEnabled(false);
                     });
                 }
             }
             else {
-                animationCheckBox = findViewById(R.id.anim_active_crashlytics);
-                animationCheckBox.setAnimation("circle_red_button.json");
-                animationCheckBox.loop(true);
-                animationCheckBox.playAnimation();
+                noteCrashlytics = findViewById(R.id.notes_crashlytics);
+                noteCrashlytics.setVisibility(View.VISIBLE);
             }
         }
 
